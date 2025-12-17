21b87597427639e1bf782fdd97e84db235b5bff0
monupal1122 <monupalji1122@gmail.com>
2025-12-17 17:27:36 +0530

    update frontend1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "../src/component/dashboard";
 import Categories from "../src/component/Categories";
 import Subcategories from "../src/component/Subcategories";
@@ -11,25 +11,23 @@ import Login from "../src/component/Login";
 import Layout from "../src/component/Layout";
 
 function App() {
-  const location = useLocation();
-  const isLoginPage = location.pathname === "/login";
-
   return (
     <Routes>
-      {isLoginPage ? (
-        <Route path="/" element={<Login />} />
-      ) : (
-        <>
-        <Route path="/" element={<Login />} />
-          <Route path="/admin-dashboard" element={<Layout><Dashboard /></Layout>} />
-          <Route path="/products" element={<Layout><Products /></Layout>} />
-          <Route path="/categories" element={<Layout><Categories /></Layout>} />
-          <Route path="/subcategories" element={<Layout><Subcategories /></Layout>} />
-          <Route path="/users" element={<Layout><Users /></Layout>} />
-          <Route path="/orderpage" element={<Layout><Order /></Layout>} />
-          <Route path="/banners" element={<Layout><Banners /></Layout>} />  
-        </>
-      )}
+      {/* Login Page */}
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Admin Pages with Layout */}
+      <Route path="/admin-dashboard" element={<Layout><Dashboard /></Layout>} />
+      <Route path="/products" element={<Layout><Products /></Layout>} />
+      <Route path="/categories" element={<Layout><Categories /></Layout>} />
+      <Route path="/subcategories" element={<Layout><Subcategories /></Layout>} />
+      <Route path="/users" element={<Layout><Users /></Layout>} />
+      <Route path="/orderpage" element={<Layout><Order /></Layout>} />
+      <Route path="/banners" element={<Layout><Banners /></Layout>} />
+
+      {/* Catch-all: redirect unknown URLs to login */}
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
